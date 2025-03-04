2fce417d4b119d3b194d3a24078a23252d408e29
AllenLiDeveloper <allen.li.service@gmail.com>
2025-03-04 22:28:39 +0800

    feat: initial setup with TypeScript and Nodejs

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,10 +3,34 @@ const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'simple-chirpstack-backend',
   projenrcTs: true,
-
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  deps: [
+    // API & Server
+    'express',
+    '@types/express'
+  ],
   // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [
+    // Type definitions
+    '@types/express',
+    '@types/jest',
+
+    // Build tools
+    'esbuild',
+
+    // Testing
+    'jest',
+    'ts-jest',
+
+    // Development utilities
+    'nodemon',
+    'prettier'
+  ],
+  jest: true
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
+});
+project.addTask('dev', {
+  exec: 'node esbuild.mjs && node dist/index.js'
 });
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import express, { Express } from 'express';
+
+class APIService {
+  private app: Express;
+  private port: number;
+
+  constructor() {
+    this.app = express();
+    this.port = 8080;
+    this.app.get('/', (_req, res) => {
+      res.send('OK');
+    });
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running at http://localhost:${this.port}`);
+    });
   }
 }
+
+export const server = new APIService();
+server.start();
